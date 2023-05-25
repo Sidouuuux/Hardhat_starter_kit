6a15e79f8dcd54ddd4fad44c4bc36cbe379527ea
Sidouuuux <mirsidahmed@hotmail.fr>
2023-05-25 15:26:51 +0400

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,59 +1,123 @@
 require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("./tasks/block-number")
 require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
+require("hardhat-deploy")
 require("solidity-coverage")
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const { COINMARKETCAP_API_KEY, BSC_RPC_URL, SEPOLIA_RPC_URL, MUMBAI_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
-const apiKeys = {
-  COINMARKETCAP_API_KEY: COINMARKETCAP_API_KEY || "",
-  BSC_RPC_URL: BSC_RPC_URL || "",
-  SEPOLIA_RPC_URL: SEPOLIA_RPC_URL || "",
-  MUMBAI_RPC_URL: MUMBAI_RPC_URL || "",
-  PRIVATE_KEY: PRIVATE_KEY || "",
-  ETHERSCAN_API_KEY: ETHERSCAN_API_KEY || ""
-};
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.g.alchemy.com/v2/YOUR-API-KEY"
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL ||
+  "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY =
+  process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS.toLowerCase() === "true" || false
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    bsc: {
-      url: apiKeys.BSC_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 97,
-    },
-    sepolia: {
-      url: apiKeys.SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
-    },
-    mumbai: {
-      url: apiKeys.SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 80001,
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545/",
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // },
       chainId: 31337,
     },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmation: 6,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmation: 6,
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 1,
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 137,
+    },
   },
-  solidity: "0.8.8",
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+      polygon: POLYGONSCAN_API_KEY,
+    },
   },
   gasReporter: {
-    enabled: true,
+    enabled: REPORT_GAS,
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["OurToken"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    user1: {
+      default: 1,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
   },
 }
